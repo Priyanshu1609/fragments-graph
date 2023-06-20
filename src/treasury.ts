c4898edc7c1eed5ec176bde071ce2e42d49bc775
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2023-06-20 11:39:26 +0530

    init

---
+++ src/treasury.ts
@@ -0,0 +1,118 @@
+import {
+  AdminChanged as AdminChangedEvent,
+  BeaconUpgraded as BeaconUpgradedEvent,
+  FundsDeposited as FundsDepositedEvent,
+  FundsWithdrawn as FundsWithdrawnEvent,
+  Initialized as InitializedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Upgraded as UpgradedEvent
+} from "../generated/Treasury/Treasury"
+import {
+  AdminChanged,
+  BeaconUpgraded,
+  FundsDeposited,
+  FundsWithdrawn,
+  Initialized,
+  OwnershipTransferred,
+  Upgraded
+} from "../generated/schema"
+
+export function handleAdminChanged(event: AdminChangedEvent): void {
+  let entity = new AdminChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousAdmin = event.params.previousAdmin
+  entity.newAdmin = event.params.newAdmin
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
+  let entity = new BeaconUpgraded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.beacon = event.params.beacon
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleFundsDeposited(event: FundsDepositedEvent): void {
+  let entity = new FundsDeposited(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.sender = event.params.sender
+  entity.token = event.params.token
+  entity.amount = event.params.amount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleFundsWithdrawn(event: FundsWithdrawnEvent): void {
+  let entity = new FundsWithdrawn(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.receiver = event.params.receiver
+  entity.initiator = event.params.initiator
+  entity.token = event.params.token
+  entity.amount = event.params.amount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleInitialized(event: InitializedEvent): void {
+  let entity = new Initialized(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.version = event.params.version
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleUpgraded(event: UpgradedEvent): void {
+  let entity = new Upgraded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.implementation = event.params.implementation
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
